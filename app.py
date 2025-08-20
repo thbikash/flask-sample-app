2af8071269fa24469229b749fa23122df1d0d2ab
Bikash Thapa <bikashthazure2025@gmail.com>
2025-08-20 14:04:49 -0400

    app code update

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! from Flask Sample Application!!!"
-    return "Version 1.0"
+    return "Hello! from Flask Sample Application V1.0!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081, debug=True)
